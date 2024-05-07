25f03a7069d1c0883ddd5b691e883442a3cbc92e
Katja Hvid Krogh <122166841+Katjahkrogh@users.noreply.github.com>
2024-05-07 10:49:03 +0200

    done

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,16 +1,48 @@
-import React from 'react'
-import Startpage from './Startpage'
-import LogInd from './logInd'
-import OpretBruger from './OpretBruger'
+'use client';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import Startpage from './Startpage';
+import LogInd from './logInd';
+import OpretBruger from './OpretBruger';
 
 function Wrapper() {
+  const [booking, setBooking] = useState([]);
+
+  useEffect(() => {
+    const fetchBookingData = async () => {
+      const url = `https://nckxtdsipzwbtkcrrjbe.supabase.co/rest/v1/Tider?select=*`;
+      const options = {
+        method: 'GET',
+        headers: {
+          apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ja3h0ZHNpcHp3YnRrY3JyamJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ5OTY4NzgsImV4cCI6MjAzMDU3Mjg3OH0.YvIHTrtBTrOQiKB79QaqdOT5iOxpyeui20rfJ5t2CdQ',
+        },
+      };
+      const res = await fetch(url, options);
+      const data = await res.json();
+      setBooking(data);
+    };
+
+    fetchBookingData();
+  }, []);
   return (
-    <div className='px-10'>
+    <div className="px-10">
+      <div>
+        <ul>
+          {booking.map((book) => (
+            <li key={book.id}>{book.day}</li>
+          ))}
+        </ul>
+        <ul>
+          {booking.map((book) => (
+            <li key={book.id}>{book.time}</li>
+          ))}
+        </ul>
+      </div>
       <Startpage />
       <LogInd />
       <OpretBruger />
     </div>
-  )
+  );
 }
 
-export default Wrapper
+export default Wrapper;
