df1aabfcd4ecde277c0ede40ded878558a4b01ba
Katja Hvid Krogh <122166841+Katjahkrogh@users.noreply.github.com>
2024-05-07 14:47:03 +0200

    kalender

--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -1,5 +1,6 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
 import { add, eachDayOfInterval, endOfMonth, format, getDay, isEqual, isSameDay, isSameMonth, isToday, parse, startOfToday } from 'date-fns';
+import { da } from 'date-fns/locale';
 import { Fragment, useState, useEffect } from 'react';
 
 function classNames(...classes) {
@@ -48,12 +49,12 @@ export default function Example() {
   let selectedDayMeetings = meetings.filter((meeting) => isSameDay(parse(meeting.day, 'yyyy-MM-dd', new Date()), selectedDay));
 
   return (
-    <div className="pt-16">
+    <div className="py-16">
       <div className="max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6">
         <div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200">
           <div className="md:pr-14">
             <div className="flex items-center">
-              <h2 className="flex-auto font-semibold text-gray-900">{format(firstDayCurrentMonth, 'MMMM yyyy')}</h2>
+              <h2 className="flex-auto font-semibold text-gray-900 capitalize">{format(firstDayCurrentMonth, 'MMMM yyyy', { locale: da })}</h2>
               <button type="button" onClick={previousMonth} className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500">
                 <span className="sr-only">Previous month</span>
                 <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
@@ -78,8 +79,7 @@ export default function Example() {
                   <button type="button" onClick={() => setSelectedDay(day)} className={classNames(isEqual(day, selectedDay) && 'text-white', !isEqual(day, selectedDay) && isToday(day) && 'text-pink-500', !isEqual(day, selectedDay) && !isToday(day) && isSameMonth(day, firstDayCurrentMonth) && 'text-gray-900', !isEqual(day, selectedDay) && !isToday(day) && !isSameMonth(day, firstDayCurrentMonth) && 'text-gray-400', isEqual(day, selectedDay) && isToday(day) && 'bg-pink-500', isEqual(day, selectedDay) && !isToday(day) && 'bg-green', !isEqual(day, selectedDay) && 'hover:bg-gray-200', (isEqual(day, selectedDay) || isToday(day)) && 'font-semibold', 'mx-auto flex h-8 w-8 items-center justify-center rounded-full')}>
                     <time dateTime={format(day, 'yyyy-MM-dd')}>{format(day, 'd')}</time>
                   </button>
-
-                  <div className="w-1 h-1 mx-auto mt-1">{meetings.some((meeting) => isSameDay(parse(meeting.startDatetime, "yyyy-MM-dd'T'HH:mm:ss", new Date()), day)) && <div className="w-1 h-1 rounded-full bg-sky-500"></div>}</div>
+                  <div className="w-1 h-1 mx-auto mt-1">{meetings.some((meeting) => isSameDay(parse(meeting.day, 'yyyy-MM-dd', new Date()), day)) && <div className="w-1 h-1 rounded-full bg-pink-500"></div>}</div>
                 </div>
               ))}
             </div>
@@ -87,7 +87,9 @@ export default function Example() {
           <section className="mt-12 md:mt-0 md:pl-14">
             <h2 className="font-semibold text-gray-900">
               Ledige tider
-              <time dateTime={format(selectedDay, 'yyyy-MM-dd')}>{format(selectedDay, ' dd. MMM')}</time>
+              <time className="capitalize" dateTime={format(selectedDay, 'yyyy-MM-dd')}>
+                {format(selectedDay, ' dd. MMM', { locale: da })}
+              </time>
             </h2>
             <ol className="mt-4 space-y-1 text-sm leading-6 text-gray-500">{selectedDayMeetings.length > 0 ? selectedDayMeetings.map((meeting) => <Meeting meeting={meeting} key={meeting.id} />) : <p>Ingen ledige tider</p>}</ol>
           </section>
@@ -99,13 +101,14 @@ export default function Example() {
 
 function Meeting({ meeting }) {
   let startDateTime = new Date(`${meeting.day}T${meeting.time}`);
+  let formattedDateTime = format(startDateTime, 'HH:mm', { timeZone: 'Europe/Copenhagen' });
 
   return (
     <li className="flex items-center px-4 py-2 space-x-4 group rounded-xl focus-within:bg-gray-100 hover:bg-gray-100">
       <div className="flex-auto">
         <p className="text-gray-900">{meeting.name}</p>
         <p className="mt-0.5">
-          <time dateTime={startDateTime.toISOString()}>{format(startDateTime, 'hh:mm')}</time>
+          <time dateTime={startDateTime.toISOString()}>{formattedDateTime}</time>
         </p>
       </div>
     </li>
