5e2a9c6c1219bb4d930400c29b05e0f18c198d16
Hannah2609 <hann7419@stud.kea.dk>
2024-05-29 23:55:30 +0200

    tooltip

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import BackBtn from './BackBtn';
 import { format } from 'date-fns';
 import { da } from 'date-fns/locale';
+import Tooltip from "./Tooltip";
 
 function Overview({ selectedBooking, selectedTreatment, setStep }) {
   return (
@@ -13,8 +14,8 @@ function Overview({ selectedBooking, selectedTreatment, setStep }) {
         <BackBtn setStep={setStep} />
       </div>
       <StepText
-        header={'Bekræft din booking'}
-        smallText={'Du modtager en mail med endelig bekræftelse'}
+        header={"Bekræft din booking"}
+        smallText={"Du modtager en mail med endelig bekræftelse"}
       />
 
       {selectedBooking && (
@@ -23,25 +24,30 @@ function Overview({ selectedBooking, selectedTreatment, setStep }) {
           <div className="bg-lightBeige w-full md:w-[500px] rounded-xl py-6 px-6 flex flex-col gap-4">
             <p className="text-lg">{selectedTreatment}</p>
             <div className="flex gap-6 text-lightGreen text-sm">
-              <p>{format(new Date(selectedBooking.day), ' dd. MMMM', { locale: da })}</p>
+              <p>
+                {format(new Date(selectedBooking.day), " dd. MMMM", {
+                  locale: da,
+                })}
+              </p>
               <p>{selectedBooking.time}</p>
             </div>
           </div>
         </div>
       )}
-
-      <Link className="w-full md:w-[500px] underline" href={''}>
-        Bookingspolitik
-      </Link>
+      <div className="flex items-center w-full md:w-[500px]">
+        <p >Aflysningspolitik</p>
+        <Tooltip text={"Aflysning skal senest 24 timer inden. Du skal aflyse via DM på Instagram eller via telefon."} />
+      </div>
 
       <div className="flex items-top w-full md:w-[500px] ">
         <label
-          htmlFor={'politik'}
-          className="cursor-pointer rounded-xl py-4 pl-4 text-xs md:text-sm text-lightGreen relative flex items-center">
+          htmlFor={"politik"}
+          className="cursor-pointer rounded-xl py-4 pl-4 text-xs md:text-sm text-lightGreen relative flex items-center"
+        >
           <input
             type="checkbox"
             name="politik"
-            id={'politik'}
+            id={"politik"}
             className="before:content[''] peer -ml-4 mr-4 mt-2 relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-lighGreen transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-10 before:w-10 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-pink-500 checked:bg-pink-500 checked:before:bg-pink-500 hover:before:bg-pink-500 hover:before:opacity-10"
           />
           <div className="pointer-events-none absolute top-[57%] left-[2.5%] -translate-y-2/4 -translate-x-2/4 text-white opacity-0 transition-opacity peer-checked:opacity-100">
@@ -51,21 +57,23 @@ function Overview({ selectedBooking, selectedTreatment, setStep }) {
               viewBox="0 0 20 20"
               fill="currentColor"
               stroke="currentColor"
-              strokeWidth={1}>
+              strokeWidth={1}
+            >
               <path
                 fillRule="evenodd"
                 d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                clipRule="evenodd"></path>
+                clipRule="evenodd"
+              ></path>
             </svg>
           </div>
-          Jeg har læst og accepterer bookings og afbudspolitikken
+          Jeg har læst og accepterer afbudspolitikken
         </label>
       </div>
       <PrimaryBtn
         setStep={setStep}
-        text={'Bekræft din booking'}
-        type={'submit'}
-        id={'bookingForm'}
+        text={"Bekræft din booking"}
+        type={"submit"}
+        id={"bookingForm"}
       />
     </div>
   );
